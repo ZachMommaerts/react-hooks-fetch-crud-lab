edfb8d862bbed8329495bea47dd9ee83c8332812
Zach <zmommaerts@gmail.com>
2021-12-02 20:53:41 -0600

    stuck

--- src/components/App.js
+++ src/components/App.js
@@ -1,15 +1,24 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import AdminNavBar from "./AdminNavBar";
 import QuestionForm from "./QuestionForm";
 import QuestionList from "./QuestionList";
 
 function App() {
   const [page, setPage] = useState("List");
+  const [questions, setQuestions] = useState([])
+
+  const fetchQuestions = () => {
+    fetch('http://localhost:4000/questions')
+    .then(r => r.json())
+    .then(questionList => setQuestions(questionList))
+  }
+  useEffect(fetchQuestions, [])
+
 
   return (
     <main>
       <AdminNavBar onChangePage={setPage} />
-      {page === "Form" ? <QuestionForm /> : <QuestionList />}
+      {page === "Form" ? <QuestionForm questions={questions} setQuestions={setQuestions}/> : <QuestionList questions={questions} />}
     </main>
   );
 }
